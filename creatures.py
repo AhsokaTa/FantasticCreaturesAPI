b6f6ab02d58550741305cfc44b8460763c35a9b7
AhsokaTa <sanchz.cris@gmail.com>
2023-06-20 12:52:51 +0200

    Initial commit: Set up project structure for Fantastic Creatures application

---
+++ creatures.py
@@ -0,0 +1,11 @@
+creatures = [
+    {"name" : "Wookiee", "tamaño" : "2.2" , "caracteristica": "la Gente de los Árboles, eran una especie\
+                                            de peludos humanoides bípedos que habitaban en el planeta Kashyyyk. "},
+    {"name" : "Ewok" , "tamaño" : "1" , "caracteristica": "completamente cubiertos de pelo normalmente de tonos\
+                                         oscuros y son muy bonitos. Poseían unos grandes ojos negros y una boca pequeña. Sus manos solo tenían tres dedos bastante prensiles para escalar a los árboles pero carecían de garras." },
+    {"name" : "Jawas", "tamaño" : "1" , "caracteristica": "pertenecen a tribus expandidas por todo Tatooine,\
+                                        donde viven en familias de hasta 10 o 15 miembros. Son capaces de reparar\
+                                        y desmontar droides astromecánicos y conducir un vehículo llamado\
+                                        Sand Crawler (reptador de las arenas, en español) para ir de un lugar a otro;\
+                                        son nómadas." }
+]
---
+++ readme.md
@@ -0,0 +1,12 @@
+# Criaturas fantásticas
+
+## Descripción
+
+API sobre criaturas fantásticas, implementa un CRUD para interactuar con una base de datos y proporcionar una interfaz de una API web. Permite realizar operaciones básicas de creación, lectura, actualización y eliminación de recursos.
+
+La API está desarrollada en Python utilizando el framework Flask. Se ha diseñado la lógica necesaria para realizar las operaciones CRUD en una base de datos y se proporciona una interfaz HTTP para interactuar con los recursos.
+
+# Documentación de la API
+
+Para probar la API y realizar solicitudes HTTP, puedes utilizar la herramienta Insomnia. Puedes descargar Insomnia desde su sitio web oficial (https://insomnia.rest/) e importar la colección de solicitudes de ejemplo proporcionada en el archivo insomnia_collection.json
+
