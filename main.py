27ea81c6aa2102ff95c6c77486cbc9998b5c1857
AhsokaTa <sanchz.cris@gmail.com>
2023-07-07 10:16:51 +0200

    testing

--- main.py
+++ main.py
@@ -2,5 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+from creatures import creatures
+
+@app.route('/')
+def hello():
+    return "Hello world"
+
 if __name__ == '__main__' : 
     app.run (debug = True)
